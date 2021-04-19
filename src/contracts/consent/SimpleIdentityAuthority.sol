1dcbf42a3f77de47a31ae61d8b3c03103de83d3a
james-c <j.a.cunningham@gmail.com>
2021-04-19 15:54:32 +0100

    Add Consent contracts

---
+++ src/contracts/consent/SimpleIdentityAuthority.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.0;
+
+import "./SelfSigningIdentityAuthority.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+
+contract SimpleIdentityAuthority is SelfSigningIdentityAuthority, Ownable {
+
+    event SignerAdded(address indexed _signer);
+    event SignerRevoked(address indexed _signer);
+
+    mapping (address => bool) internal signers_;
+
+    function addSigner(address _signer) public onlyOwner returns (bool) {
+        signers_[_signer] = true;
+        emit SignerAdded(_signer);
+        return true;
+    }
+
+    function revokeSigner(address _signer) public onlyOwner returns (bool) {
+        signers_[_signer] = false;
+        emit SignerRevoked(_signer);
+        return true;
+    }
+
+    function canSign(address _for, address _signer) public override virtual returns (bool) {
+        if (signers_[_signer]) {
+            return true;
+        }
+
+        return super.canSign(_for, _signer);
+    }
+
+    
+}
