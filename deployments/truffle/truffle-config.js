4706be575c8e5bd897b42638466ab5245b023c65
james-c <j.a.cunningham@gmail.com>
2021-04-19 11:31:46 +0100

    Add truffle deployment setup

---
+++ deployments/truffle/truffle-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+    contracts_build_directory: process.env.CONTRACTS_DIR || "./build/contracts",
+    
+    networks: {
+        development: {
+            host: process.env.GANACHE_HOST,
+            port: process.env.GANACHE_PORT,
+            network_id: process.env.GANACHE_NETWORK,
+            accounts: 5,
+            defaultEtherBalance: 500
+        }
+    },
+
+    compilers: {
+        solc: {
+            version: SOLC_VERSION,
+            settings: {
+                optimizer: {
+                    enabled: true
+                },
+                evmVersion: "istanbul"
+            }
+        }
+    }
+}
