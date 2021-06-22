47646f7ae2766285d6a3ec0c55f33e41bc139466
James Cunningham <j.a.cunningham@gmail.com>
2021-06-22 12:34:33 +0100

    Keep truffle box container running

--- deployments/dev/scripts/truffle-compile-and-migrate.sh
+++ deployments/dev/scripts/truffle-compile-and-migrate.sh
@@ -2,4 +2,5 @@
 
 npm install > "/dev/null" 2>&1 &&
     truffle compile --contracts_directory ./migration_contract --contracts_build_directory /build --network development &&
-    truffle migrate --contracts_build_directory /build --network development
+    truffle migrate --contracts_build_directory /build --network development &&
+    tail -f /dev/null
